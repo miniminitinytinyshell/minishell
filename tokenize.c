c4f34e145f893a6f50891d6a6b3fd90b99c320f8
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-03-21 15:28:20 +0900

    feat: tokenizer 최종

--- tokenize.c
+++ tokenize.c
@@ -6,7 +6,7 @@
 /*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 17:33:19 by jaeblee           #+#    #+#             */
-/*   Updated: 2024/03/21 14:42:28 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/03/21 15:27:06 by hyeunkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,76 +14,29 @@
 #include "function.h"
 #include "libft.h"
 
-int	token_len_meta(char *str)
-{
-	int	len;
-
-	if (str[0] == '(' || str[0] == ')')
-		len = 1;
-	else
-	{
-		len = 0;
-		while (str[len] == str[0])
-			len++;
-	}
-	return (len);
-}
-
-int	token_len_word(char *str)
-{
-	int		len;
-	char	quote_type;
-
-	len = 0;
-	while (str[len])
-	{
-		if (str[len] == '\'' || str[len] == '"')
-		{
-			quote_type = str[len];
-			len++;
-			while (str[len] != quote_type)
-				len++;
-		}
-		len++;
-		if (ft_strchr("<>()|& ", str[len]))
-			break ;
-	}
-	return (len);
-}
-
-int	quoted_len(char *str)
-{
-	int		len;
-	char	quote_type;
-
-	quote_type = str[0];
-	len = 1;
-	while (str[len] && str[len] != quote_type)
-		len++;
-	return (len);
-}
-
 int	token_len(char *str)
 {
 	int		len;
+	char	quote_type;
 
 	len = 0;
 	if (str[0] == '(' || str[0] == ')')
 		len = 1;
 	else if (ft_strchr("<>|&", str[0]))
-	{
 		while (str[len] == str[0])
 			len++;
-	}
 	else
 	{
-		while (str[len])
+		while (str[len] && !ft_strchr("()<>|& ", str[len]))
 		{
 			if (str[len] == '\'' || str[len] == '"')
-				len += quoted_len(str + len);
+			{
+				quote_type = str[len];
+				len++;
+				while (str[len] && str[len] != quote_type)
+					len++;
+			}
 			len++;
-			if (ft_strchr("()<>|& ", str[len]))
-				break ;
 		}
 	}
 	return (len);
@@ -98,10 +51,7 @@ void	tokenizer(t_token **token, char *str)
 	{
 		while (*str == ' ')
 			str++;
-		if (ft_strchr("()<>|&", *str))
-			len = token_len_meta(str);
-		else
-			len = token_len_word(str);
+		len = token_len(str);
 		if (*str == '(' || *str == ')')
 			new = token_new(str, len, sep);
 		else if (*str == '|' || *str == '&')
