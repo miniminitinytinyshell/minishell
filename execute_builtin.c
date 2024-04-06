e1ad9fa928ca6f4291d7f92d438fee2d99e908d5
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-04-06 18:50:53 +0900

    fix: execute_builtin에서pwd 찾을 수 있도록 주석 해제

--- execute_builtin.c
+++ execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:57:10 by jaeblee           #+#    #+#             */
-/*   Updated: 2024/04/06 15:12:19 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/04/06 18:50:32 by hyeunkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ void	execute_builtin(t_tree *tree, char **envp, int *status)
 		*status =  builtin_echo(tree->right->data);
 	else if (ft_strncmp(tree->right->data[0], "cd", 3) == 0)
 		*status = builtin_cd(tree->right->data, envp);
-	// else if (ft_strncmp(tree->right->data[0], "pwd", 4) == 0)
-	// 	*status = builtin_pwd();
+	else if (ft_strncmp(tree->right->data[0], "pwd", 4) == 0)
+		*status = builtin_pwd();
 	// else if (ft_strncmp(tree->right->data[0], "export", 7) == 0)
 	// 	*status = builtin_export();
 	// else if (ft_strncmp(tree->right->data[0], "unset", 6) == 0)
