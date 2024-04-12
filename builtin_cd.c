f4b3b090735a34bf9d635ed8980f949b374a2cfb
OortC <jaebin.lee00@gmail.com>
2024-04-12 18:53:24 +0900

    fix: cd 오류 출력 및 자잘한 수정

--- builtin_cd.c
+++ builtin_cd.c
@@ -3,31 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaeblee <jaeblee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:16:58 by hyeunkim          #+#    #+#             */
-/*   Updated: 2024/04/12 18:30:09 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/04/12 18:51:40 by jaeblee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "struct.h"
 #include "function.h"
-
-// static int	cd_getcwd_error(int flag)
-// {
-// 	if (flag == 0)
-// 	{
-// 		ft_putstr_fd("minishell: cd: getcwd: ", STDERR_FILENO);
-// 		ft_putendl_fd(strerror(errno), STDERR_FILENO);
-// 	}
-// 	else
-// 	{
-// 		ft_putstr_fd("cd: error retrieving current directory: ", STDERR_FILENO);
-// 		ft_putstr_fd("getcwd: cannot access parent directories: ", STDERR_FILENO);
-// 		ft_putendl_fd("No such file or directory", STDERR_FILENO);
-// 	}
-// 	return (EXIT_FAILURE);
-// }
 
 int	cd_home_error(void)
 {
@@ -37,9 +21,19 @@ int	cd_home_error(void)
 
 int	cd_pwd_error(char *path)
 {
-	ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-	ft_putstr_fd(path, STDERR_FILENO);
-	ft_putendl_fd(": No such file of directory", STDERR_FILENO);
+	if (path)
+	{
+		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+		ft_putstr_fd(path, STDERR_FILENO);
+		ft_putendl_fd(": No such file of directory", STDERR_FILENO);
+	}
+	else
+	{
+		ft_putstr_fd("cd: error retrieving current directory: ", STDERR_FILENO);
+		ft_putstr_fd("getcwd: ", STDERR_FILENO);
+		ft_putstr_fd("cannot access parent directories: ", STDERR_FILENO);
+		ft_putendl_fd("No such file or directory", STDERR_FILENO);
+	}
 	return (EXIT_FAILURE);
 }
 
@@ -49,16 +43,18 @@ void	set_pwd(char *path, t_envp *envp)
 	char	*old_path;
 
 	old_path = envp->pwd;
+	swap_envp_data("OLDPWD", old_path, envp);
 	tmp_path = getcwd(NULL, 0);
 	if (!tmp_path)
 	{
-		if (old_path[ft_strlen(old_path)] != '/')
+		cd_pwd_error(NULL);
+		if (old_path[ft_strlen(old_path) - 1] != '/')
 			old_path = strjoin_char(old_path, '/');
 		tmp_path = ft_strjoin(old_path, path);
 	}
-	swap_envp_data("OLDPWD", old_path, envp);
 	swap_envp_data("PWD", tmp_path, envp);
 	envp->pwd = tmp_path;
+	free(old_path);
 }
 
 char	*set_rel_path(char *pwd, char *target)
@@ -77,31 +73,24 @@ char	*set_rel_path(char *pwd, char *target)
 	return (rel_path);
 }
 
-char	*set_path(char *arg1, t_envp *envp)
-{
-	char	*path;
-
-	if (!arg1)
-		path = ft_strchr(envp->data[get_envp_idx("HOME", envp)], '=') + 1;
-	else
-	{
-		path = set_rel_path(envp->pwd, arg1);
-		if (access(path, F_OK) < 0)
-			path = ft_strdup(arg1);
-	}
-	return (path);
-}
-
-// 우선 단순하게 문제 없는 상황에 대해서만 만들고,
-// 상위디렉토리가 삭제되는 경우에는 다른 함수 호출하게 만들어보자...
 int	builtin_cd(char **args, t_envp *envp)
 {
 	int		result;
 	char	*path;
 
-	path = set_path(args[1], envp);
+	if (!args[1])
+		path = ft_strchr(envp->data[get_envp_idx("HOME", envp)], '=') + 1;
+	else
+	{
+		path = set_rel_path(envp->pwd, args[1]);
+		if (access(path, F_OK) < 0)
+			path = ft_strdup(args[1]);
+	}
 	result = chdir(path);
-	set_pwd(path, envp);
+	if (result == 0)
+		set_pwd(path, envp);
+	else
+		cd_pwd_error(path);
 	free(path);
 	return (result);
 }
--- test/test_cd.c
+++ test/test_cd.c
@@ -6,7 +6,7 @@
 /*   By: jaeblee <jaeblee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 17:46:37 by jaeblee           #+#    #+#             */
-/*   Updated: 2024/04/12 17:55:35 by jaeblee          ###   ########.fr       */
+/*   Updated: 2024/04/12 18:38:55 by jaeblee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,19 @@
 
 int main(void)
 {
+	int		check;
 	char	*temp;
 	char	*cmd[] = {"ls", NULL};
 
 	temp = getcwd(NULL, 0);
 	printf("pwd: %s\n", temp);
-	chdir("/Users/jaeblee");
+	check = chdir("..");
+	printf("check = %d\n", check);
 	temp = getcwd(NULL, 0);
 	printf("pwd: %s\n", temp);
-	chdir("goinfre");
+	check = chdir("..");
+	printf("check = %d\n", check);
 	temp = getcwd(NULL, 0);
 	printf("pwd: %s\n", temp);
-	execve("/bin/ls", cmd, NULL);
 	return (0);
 }
