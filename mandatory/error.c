addc762a0132e29a2bbcd6015cf8408cd29ac27c
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-04-17 18:07:18 +0900

    fix: error_syntax l_sep인지 r_sep인지 판단

--- mandatory/error.c
+++ mandatory/error.c
@@ -6,7 +6,7 @@
 /*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 18:12:24 by jaeblee           #+#    #+#             */
-/*   Updated: 2024/04/17 14:18:45 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/04/17 18:06:36 by hyeunkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,38 @@ int	error_with_str(char *str, int flag)
 		return (1);
 }
 
+static char	count_sep(t_token **token, int flag)
+{
+	t_token	*cur;
+	int		l_sep;
+	int		r_sep;
+
+	cur = *token;
+	l_sep = 0;
+	r_sep = 0;
+	while (cur)
+	{
+		if (cur->data[0] == '(')
+			l_sep++;
+		if (cur->data[0] == ')')
+			r_sep++;
+		cur = cur->next;
+	}
+	if (l_sep > r_sep)
+		return ('(');
+	else if (l_sep < r_sep)
+		return (')');
+	else
+		return (flag);
+}
+
 int	error_syntax(char *str, t_token **token, int flag)
 {
 	ft_putstr_fd("mongshell🐶: ", STDERR_FILENO);
 	ft_putstr_fd("syntax error near unexpected token ", STDERR_FILENO);
 	ft_putchar_fd('\'', STDERR_FILENO);
+	if (flag == '(')
+		flag = (int)count_sep(token, flag);
 	if (flag)
 		ft_putchar_fd((char)flag, STDERR_FILENO);
 	else
