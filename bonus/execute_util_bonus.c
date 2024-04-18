24b4ce11e18b8128d11d7fe0208919116f81cfdd
OortC <jaebin.lee00@gmail.com>
2024-04-18 15:35:46 +0900

    fix: rdr empty case

--- bonus/execute_util_bonus.c
+++ bonus/execute_util_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_util_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaeblee <jaeblee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 14:37:07 by jaeblee           #+#    #+#             */
-/*   Updated: 2024/04/17 15:02:46 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/04/18 15:35:32 by jaeblee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,27 +26,34 @@ void	set_status(int *status)
 		*status = WEXITSTATUS(*status);
 }
 
-int	open_file(t_tree *tree, int *file_in, int *file_out)
+static void	proc_open(t_tree *tree, int *file_in, int *file_out)
 {
-	if (!tree)
-		return (0);
-	if (ft_strncmp(tree->left->data[0], "<", 1) == 0)
+	if (ft_strncmp(tree->data[0], "<", 1) == 0)
 	{
 		if (*file_in > 0)
 			close(*file_in);
-		*file_in = open(tree->left->data[1], O_RDONLY);
+		*file_in = open(tree->data[1], O_RDONLY);
 	}
 	else
 	{
 		if (*file_out > 0)
 			close(*file_out);
-		if (ft_strncmp(tree->left->data[0], ">", 2) == 0)
-			*file_out = open(tree->left->data[1], \
+		if (ft_strncmp(tree->data[0], ">", 2) == 0)
+			*file_out = open(tree->data[1], \
 			O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		else
-			*file_out = open(tree->left->data[1], \
+			*file_out = open(tree->data[1], \
 			O_WRONLY | O_CREAT | O_APPEND, 0644);
 	}
+}
+
+int	open_file(t_tree *tree, int *file_in, int *file_out)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left->data[1])
+		return (error_with_str(NULL, AMB_RDR));
+	proc_open(tree->left, file_in, file_out);
 	if (*file_in < 0)
 		return (error_with_str(tree->left->data[1], 0));
 	else if (*file_out < 0)
