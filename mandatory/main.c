7c05d268c66d0a5b8a086afbebb357015e91c655
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-04-23 16:04:16 +0900

    fix: 프롬프트 출력

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:35:56 by jaeblee           #+#    #+#             */
-/*   Updated: 2024/04/23 16:03:51 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:04:07 by hyeunkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ int	main(int argc, char **argv, char **envp)
 	{
 		g_signum = 0;
 		set_signal();
-		cmd = readline("mongshell\001🐶\002> ");
+		cmd = readline("mongshell\001🐶\002$ ");
 		if (check_cmd(cmd, &status) < 0)
 			continue ;
 		proc_shell(&env, &status, cmd);
